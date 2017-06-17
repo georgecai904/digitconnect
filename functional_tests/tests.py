930f1c2c942571be97d4346d26cee28c3efe0e8f
George Cai <georgemail608@gmail.com>
2017-06-17 10:30:27 +0800

    update FT to classbased

---
+++ functional_tests/tests.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from django.test import LiveServerTestCase, TestCase
+import time
+
+
+class FunctionalTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(FunctionalTest, cls).setUpClass()
+        cls.browser = webdriver.Chrome(executable_path="/driver/chromedriver")
+        cls.live_server_url = "http://localhost:8000"
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super(FunctionalTest, cls).tearDownClass()
+
+    def test_django_server_is_working(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn("Django", self.browser.title)
+
+    # def test_template_load_is_correct(self):
+        # self.browser.get(self.live_server_url)
+        # self.assertTemplateUsed()
