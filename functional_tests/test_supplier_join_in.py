f6308237a2a6e3d17807b25999c86ae4f9d5b624
George Cai <georgemail608@gmail.com>
2017-06-17 15:56:55 +0800

    second FT to do SupplierJoinInTest

---
+++ functional_tests/test_supplier_join_in.py
@@ -0,0 +1,17 @@
+from functional_tests.base import FunctionalTest
+
+
+class SupplierJoinInTest(FunctionalTest):
+
+    def test_supplier_join_in(self):
+
+        # 山姆是一个供应商，他登陆了网站
+
+        # 山姆看到导航栏上有"申请入驻"的标志
+        # 山姆点击进去
+
+        # 进去之后，山姆发现导航栏还在，但是页面呈现出一个表格，需要填写相关的信息
+
+        # 山姆按照需要的信息填写进去并提交
+
+        # 提交之后，网站回到首页，并在首页上方显示入驻成功
