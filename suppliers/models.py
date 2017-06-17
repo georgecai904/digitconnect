d76fbbf276292f47e5970abdae48afa06b7694d2
George Cai <georgemail608@gmail.com>
2017-06-17 16:49:34 +0800

    create supplier models

--- suppliers/models.py
+++ suppliers/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class Supplier(models.Model):
-    pass
+    name = models.TextField(default='')
+    phone = models.TextField(default='')
+    address = models.TextField(default='')
+    location = models.TextField(default='')
+    license = models.TextField(default='')
+    area = models.TextField(default='')
