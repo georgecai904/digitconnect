c8b24bee9a4204e77df0f3be78af0b656b6e3df6
George Cai <georgemail608@gmail.com>
2017-06-18 11:52:00 +0800

    new FTs for post new product

---
+++ functional_tests/test_post_product.py
@@ -0,0 +1,15 @@
+from functional_tests.base import FunctionalTest
+
+
+class PostNewProductTest(FunctionalTest):
+    pass
+
+    # 山姆成功注册后，回到首页，他看到了菜单栏最右边的 "产品发布"
+    # 山姆有一些产品向发布，于是他便点了进去
+
+    # 和之前注册一样，页面跳转到了一个表单页面
+
+    # 山姆将他想要发布的商品以此都填写进去
+    # 山姆填写并成功提交了
+
+    # 提交后，页面跳转到了首页，山姆成功的在首页的商品列表里面看到了他刚刚发布的产品
