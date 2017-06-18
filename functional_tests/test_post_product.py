c10db6ad9a761eaf3e0121ae3d71922b194f357d
George Cai <georgemail608@gmail.com>
2017-06-18 12:03:27 +0800

    finish FTs, start to build

--- functional_tests/test_post_product.py
+++ functional_tests/test_post_product.py
@@ -2,14 +2,31 @@ from functional_tests.base import FunctionalTest
 
 
 class PostNewProductTest(FunctionalTest):
-    pass
 
-    # 山姆成功注册后，回到首页，他看到了菜单栏最右边的 "产品发布"
-    # 山姆有一些产品向发布，于是他便点了进去
+    def test_post_new_product(self):
+        # 山姆成功注册后，回到首页，他看到了菜单栏最右边的 "产品发布"
+        self.browser.get('/')
+        post_product_link = self.browser.find_element_by_css_selector("nav li:last-child")
+        self.assertIn("产品发布", post_product_link.text)
 
-    # 和之前注册一样，页面跳转到了一个表单页面
+        # 山姆有一些产品向发布，于是他便点了进去
+        post_product_link.click()
+        self.assertEqual(self.browser.current_url, self.live_server_url + "/products/new")
 
-    # 山姆将他想要发布的商品以此都填写进去
-    # 山姆填写并成功提交了
+        # 和之前注册一样，页面跳转到了一个表单页面
+        form = self.browser.find_element_by_tag_name()
 
-    # 提交后，页面跳转到了首页，山姆成功的在首页的商品列表里面看到了他刚刚发布的产品
+        # 山姆将他想要发布的商品以此都填写进去
+        form.find_element_by_id('id_name').send_keys('B&O 音响')
+        form.find_element_by_id('id_image').send_keys('/images/product.jpg')
+        form.find_element_by_id('id_category').send_keys('高档音响')
+        form.find_element_by_id('id_amount').send_keys('100')
+        form.find_element_by_id('id_location').send_keys('江浙沪')
+
+        # 山姆填写并成功提交了
+        form.find_element_by_id('id_submit').click()
+
+        # 提交后，页面跳转到了首页，山姆成功的在首页的商品列表里面看到了他刚刚发布的产品
+        self.assertEqual(self.browser.current_url, self.live_server_url+"/")
+        container = self.browser.find_element_by_css_selector(".product-container:first-child")
+        self.assertEqual(container.find_element_by_css_selector(".product-name").text, 'B&O 音响')
